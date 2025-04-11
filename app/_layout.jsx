8008a9e78821f567a9605da7a1988cd5f229d51b
Dev.Hysterical <2124802010307@student.tdmu.edu.vn>
2025-04-11 15:10:36 +0700

    feat(auth): integrate Clerk authentication and add Home component
    - Added ClerkProvider to _layout.jsx for authentication context
    - Implemented SSO flow in login/index.jsx for Google authentication
    - Created Home component in home.jsx
    - Updated package.json with required dependencies for authentication
    - Added .env to .gitignore for environment variable management

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,23 +1,30 @@
 import { Stack } from "expo-router";
 import "react-native-reanimated";
 import { useFonts } from "expo-font";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { Text, View } from "react-native";
 
 export default function RootLayout() {
-  const [fontsLoaded] = useFonts({
+  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+  useFonts({
     outfit: require("../assets/fonts/Outfit-Regular.ttf"),
     "outfit-medium": require("../assets/fonts/Outfit-Medium.ttf"),
     "outfit-bold": require("../assets/fonts/Outfit-Bold.ttf"),
   });
+
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-      <Stack.Screen
-        name="login/index"
-        options={{
-          headerShown: false,
-        }}
-      />
-    </Stack>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <Stack>
+        <Stack.Screen name="index" />
+        <Stack.Screen
+          name="login/index"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
+    </ClerkProvider>
   );
 }
