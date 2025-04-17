39439361af387f4e915d20dfd46b6c49551ab754
Dev.Hysterical <2124802010307@student.tdmu.edu.vn>
2025-04-17 19:48:17 +0700

    feat(home): add 'Add new Pet' button with styling to Home component

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,9 +1,11 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
 import Header from "../../components/Home/Header";
 import Sliders from "../../components/Home/Sliders";
 import Category from "../../components/Home/Category";
 import PetListByCategory from "../../components/Home/PetListByCategory";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import Colors from "../../constants/Colors";
 
 export default function Home() {
   return (
@@ -21,7 +23,33 @@ export default function Home() {
       {/* Category + List of Pets */}
       <PetListByCategory />
 
-      {/* Add new Pet Option */}
+      {/* Add new Pet  */}
+      <TouchableOpacity style={styles.addNewPetContainer}>
+        <MaterialIcons name="pets" size={24} color="black" />
+        <Text
+          style={{
+            fontFamily: "outfit-medium",
+            fontSize: 15,
+          }}>
+          Add new Pet
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  addNewPetContainer: {
+    display: "flex",
+    flexDirection: "row",
+    gap: 10,
+    alignItems: "center",
+    padding: 18,
+    marginTop: 12,
+    borderWidth: 1,
+    borderRadius: 15,
+    backgroundColor: Colors.LIGHT_PRIMARY,
+    borderColor: Colors.BLACK,
+    justifyContent: "center",
+  },
+});
